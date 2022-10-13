a32f9cb471f4f6bbc9645edcd1baf8f1cba12732
Sune Ørnemark Lægdsmand <sune@Sunes-MacBook-Pro.local>
2022-10-13 15:33:39 +0200

    First edition of the website, just a bare layout with a draggable window

--- script.js
+++ script.js
@@ -9,4 +9,14 @@ $(function() {
         minWidth: 500,
         disable: true
     }).draggable();
+<<<<<<< HEAD
 });
+=======
+});
+
+import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js'
+const fs = new LightningFS('fs')
+
+const dir = '/test-clone'
+git.clone({ fs, http, dir, url: 'https://github.com/isomorphic-git/lightning-fs', corsProxy: 'https://cors.isomorphic-git.org' }).then(console.log)
+>>>>>>> e9b6755 (First edition of the website, just a bare layout with a draggable window)
